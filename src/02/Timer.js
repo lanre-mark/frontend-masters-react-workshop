af669a48457db57c780d0e8ff1f7fbaad410df3e
olanrewaju Makinde <lanre.developer@gmail.com>
2020-11-05 19:04:30 +0100

    exercise 3

--- src/02/Timer.js
+++ src/02/Timer.js
@@ -10,11 +10,8 @@ export const Timer = () => {
   const [state, send] = useMachine(timerMachine);
 
   // Use state.context instead
-  const { duration, elapsed, interval } = {
-    duration: 60,
-    elapsed: 0,
-    interval: 0.1,
-  };
+  const { duration, elapsed, interval } = state.context;
+  // console.log(state.context)
 
   return (
     <div
@@ -38,12 +35,12 @@ export const Timer = () => {
         </div>
         <div className="controls">
           {state.value !== 'running' && (
-            <button onClick={() => send('RESET')}>Reset</button>
+            <button onClick={() => send({ type: 'RESET'})}>Reset</button>
           )}
 
           <button
             onClick={() => {
-              // ...
+              send({ type: 'ADD_MINUTE'})
             }}
           >
             + 1:00
